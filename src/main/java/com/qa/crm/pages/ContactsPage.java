86ff5d41af4cf005253e583f5e65b5c85dc445ff
Sandeep Reddy <sandeepganadipalli@gmail.com>
2021-04-18 00:26:23 +0530

    added contacts page and updated login page

---
+++ src/main/java/com/qa/crm/pages/ContactsPage.java
@@ -0,0 +1,17 @@
+package com.qa.crm.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.crm.base.BasePage;
+
+public class ContactsPage extends  BasePage{
+
+	WebDriver driver;
+	
+	public ContactsPage(WebDriver driver) {
+		
+		this.driver=driver;
+	}
+	
+	
+}
