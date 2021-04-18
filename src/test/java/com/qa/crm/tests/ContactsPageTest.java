dd2068685dee35aa6623a56b5179d252b9792aff
Sandeep Reddy <sandeepganadipalli@gmail.com>
2021-04-18 12:26:42 +0530

    added contacts page and contactspage test

---
+++ src/test/java/com/qa/crm/tests/ContactsPageTest.java
@@ -0,0 +1,12 @@
+package com.qa.crm.tests;
+
+import org.testng.annotations.Test;
+
+public class ContactsPageTest {
+
+	@Test
+	public void titleTest() {
+		System.out.println("verifying contacts page title test");
+	}
+	
+}
